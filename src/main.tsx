b3b3795624324e69274870f99936597a950d459c
nohsiyoung94 <shtldudv@naver.com>
2026-02-11 12:42:45 +0900

    Configure Vercel deployment: update vite.config, add vercel.json, switch to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -8,8 +8,8 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
 
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
